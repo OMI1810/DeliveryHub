e5346abcdcbf962421563f438eab2d8e6f8b661d
NikVIP-G <68080495+NikVIP-G@users.noreply.github.com>
2026-04-06 22:26:18 +0300

    Mega update

--- back-nestjs/src/main.ts
+++ back-nestjs/src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  console.log("12312");
+
   // Global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
